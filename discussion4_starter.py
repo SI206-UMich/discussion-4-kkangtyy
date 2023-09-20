0858f744bc2e6f50775a5c3477d40529225b5204
Terri Kang <kangty@Terris-MacBook-Pro.local>
2023-09-20 09:41:31 -0400

    Modified

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -1,11 +1,13 @@
 class Rectangle():
+    def __init__(self, width, height):
     # Create the constructor "__init__" method
     # Arguments: width (an ingeter), height (an integer)
     # 
     # It sets an instance variable, "width" to the passed argument, width
     # It sets an instance variable, "height" to the passed argument, height
 
-    # YOUR CODE HERE
+        self.width = width
+        self.height = height
 
 
 
@@ -14,7 +16,8 @@ class Rectangle():
     # It returns a string, 
     #       "A rectangle with width ____ and height ____"
 
-    # YOUR CODE HERE
+    def __str__(self):
+        return f"A rectangle with width {self.width} and {self.height}"
 
 
 
@@ -24,7 +27,11 @@ class Rectangle():
     #       True if the width and height are positive numbers
     #       False otherwise
 
-    # YOUR CODE HERE
+    def verify_input(self):
+        if self.width>0 and self.height>0:
+            return True
+        else:
+            return False
 
 
 
@@ -33,7 +40,12 @@ class Rectangle():
     # It first verifies inputs and return "Invalid input" if they are invalid.
     # Otherwise, it returns the area of the rectangle.
 
-    # YOUR CODE HERE
+    def area(self):
+        if type(self.width)==int:
+            if type(self.height)==int:
+                return (self.width)*(self.height)
+            else:
+                return "Invalid input"
 
 
 
@@ -42,7 +54,14 @@ class Rectangle():
     # It first verifies inputs and return "Invalid input" if they are invalid.
     # Otherwise, it returns the perimeter of the rectangle.
 
-    # YOUR CODE HERE
+    def perimeter(self):
+        if type(self.width)==int:
+            if type(self.height)==int:
+                return (self.width*2)+(self.height*2)
+            else:
+                return "Invalid input"
+        else:
+            return "Invalid input"
     
 
 
